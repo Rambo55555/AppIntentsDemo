09b8f5c2e4bd7ca500df2bb1a5d13ece38bbeb50
rambohhhlan <rambohhhlan@tencent.com>
2022-08-01 14:56:59 +0800

    Initial Commit

---
+++ AppIntentsDemo/AppIntentsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentsDemoApp.swift
+//  AppIntentsDemo
+//
+//  Created by Rambo on 2022/8/1.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIntentsDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AppIntentsDemo
+//
+//  Created by Rambo on 2022/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
